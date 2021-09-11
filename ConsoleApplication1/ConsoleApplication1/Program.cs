12b6370475ab016ee92c2348063fe771b38156a0
2389-20 <2389-20@C41206>
2021-09-11 11:09:10 +0300

    Palindrome

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,9 +8,23 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static bool Palindrom(string s)
         {
+            for (int i = 1, j = s.Length - 1; i < j; i++, j--)
+                if (s[i] != s[j])
+                    return false;
+            return true;
+        }
 
+        static void Main()
+        {
+            string s;
+            Console.Write("Введите строку: ");
+            s = Console.ReadLine();
+            if (Palindrom(s))
+                Console.WriteLine("Эта строка - не Палиндром");
+            else Console.WriteLine("Эта строка - Палиндром");
+            Console.ReadLine();
         }
     }
 }
