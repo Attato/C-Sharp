6756f2f47acdd0d29bca2f32c3d2e7cd9830eef4
2389-20 <2389-20@C41207>
2021-09-18 15:18:07 +0300

    ConsoleApplication2 version: 0.4

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace ConsoleApplication2
 {
     public class Calc
@@ -11,16 +12,18 @@ namespace ConsoleApplication2
         public int Calculate(string expression)
         {
             Operaion operation = null;
-            int ?first = 0;
-            int ?second = 0;
+            int? first = null;
+            int? second = null;
+
             for (var i = 0; i < expression.Length; i++)
             {
                 var ch = expression[i];
 
                 switch (ch)
                 {
-                    case '-': operation = Add; break;
-                    case '+': operation = Sub; break;
+                    case '-': operation = Sub; break;
+                    case '+': operation = Add; break;
+                    
                     default:
                         {
                             if (!char.IsDigit(ch))
@@ -30,7 +33,7 @@ namespace ConsoleApplication2
 
                             var str = ch + "";
 
-                            while(i + 1 < expression.Length && char.IsDigit(expression[i + 1]))
+                            while (i + 1 < expression.Length && char.IsDigit(expression[i + 1]))
                             {
                                 i++;
                                 str += expression[i];
@@ -38,19 +41,20 @@ namespace ConsoleApplication2
 
                             var tmp = int.Parse(str);
 
-                            if (i > 0)
+                            if (first.HasValue)
                             {
                                 second = tmp;
                                 break;
                             }
+
                             first = tmp;
-                            break;                         
-                        } 
+                            break;
+                        }
                 }
-                if(operation != null &&
+                if (operation != null &&
                     first.HasValue &&
                     second.HasValue)
-                   
+
                 {
                     first = operation(first.Value, second.Value);
                     operation = null;
@@ -60,7 +64,7 @@ namespace ConsoleApplication2
             return first.Value;
         }
 
-        public int Add (int a, int b)
+        public int Add(int a, int b)
         {
             return a + b;
         }
